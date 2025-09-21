65c4f78af6bc2bf731cf289e191844ac51617884
Ashutosh Teotia <techgiddion16@gmail.com>
2025-09-22 00:08:01 +0530

    added blogapp in cors

--- config/ALLOWEDLIST.js
+++ config/ALLOWEDLIST.js
@@ -1,7 +1,8 @@
 const ALLOWEDLIST = [
     'http://localhost:4000',
     "http://localhost:3000",
-    'https://www.google.com'
+    'https://www.google.com',
+    'https://blogappbackend-1gaz.onrender.com'
 ]
 
 module.exports=ALLOWEDLIST;
